60a710af22ffe5db8f67a023a28b290d6600e52f
Oprea Sergiu-Daniel <84630418+sergiuop7@users.noreply.github.com>
2023-02-12 23:09:40 +0200

    Delete TS2 Lab directory

--- TS2 Lab/Lab12.m
+++
@@ -1,9 +0,0 @@
-clear variables
-
-close all;
-%% 1.Discretize the controller 𝐻𝑐(𝑠)=𝑘/𝑠 using tustin(bilinear) transformation.
-
-T = 5e-2; %sampling period
-k = 1;
-H_c = tf(k, [1 0]);
-H_c_discrete = c2d(H_c, T, 'tustin')
--- TS2 Lab/Lab1ex2.m
+++
@@ -1,8 +0,0 @@
-clc
-clear variables
-
-H = tf(2.2, [1e-3, 1]);
-nyquist(H)
-
-wc = 1.96e+03 %cut of freq
-
