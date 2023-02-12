2957b8d1d27fe9ae61b37d04e41687ab1aae0118
Oprea Sergiu-Daniel <84630418+sergiuop7@users.noreply.github.com>
2023-02-12 23:10:05 +0200

    Delete SS Lab directory

--- SS Lab/SS Lab1/ss1.m
+++
@@ -1,6 +0,0 @@
-x = -pi:0.02*pi:pi;
-plot(x,max(sin(x), cos(x)), 'c', 'LineWidth', 5);
-hold on
-plot(x,sin(x),'--b', x, cos(x), '-r' , 'LineWidth',3); 
-grid
-axis([-pi, pi, -1.1, 1.1]), set(gca,'FontSize',12)
--- SS Lab/SS Lab8/generate.m
+++
@@ -1,9 +0,0 @@
-% generate the signal x
-T0 =1; w0 = 2*pi/T0;
-Ts = 0.0005; t = -1:Ts:2;
-x = t - 0.1 - round(t);
-plot(t,x)
-
-% estimate the coefficients
-N = 8;
-ak = fsAnalysis(x, t, T0, Ts, N);
