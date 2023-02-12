7927d6e96753508770621ccd436af5d40551cc48
Oprea Sergiu-Daniel <84630418+sergiuop7@users.noreply.github.com>
2023-02-12 22:53:02 +0200

    Delete SS Lab directory

--- SS Lab/SS Lab1/imread.m
+++
@@ -1,6 +0,0 @@
-clear variables;
-
-I = imread('peppers.png'); I=rgb2gray(I);
-figure, imshow(I)
-[x,y] = meshgrid(1:size(I,2), I:size(I,1));
-figure, surf(x,y,double(I)), shading flat, colormap gray
--- SS Lab/SS Lab1/meshgrid.m
+++
@@ -1,5 +0,0 @@
-clear variables;
-
-[t,p]=meshgrid(0:.02*pi:3*pi,0:.02*pi:3*pi);
-figure, surf(t,p,t/2*sin(p)), xlabel('t'), ylabel('p'), title('t/2 sin(p)')
-view(40, 60)
